48310cc379318daa7892ade73b2a1a563d9d6e93
tedopoly <93625527+tedopoly@users.noreply.github.com>
2023-11-13 10:07:52 -0500

    Update coordinator.py

--- custom_components/ontario_energy_board/coordinator.py
+++ custom_components/ontario_energy_board/coordinator.py
@@ -15,22 +15,15 @@ from .const import (
     DOMAIN,
     RATES_URL,
     REFRESH_RATES_INTERVAL,
-    XML_KEY_OFF_PEAK_RATE,
-    XML_KEY_MID_PEAK_RATE,
-    XML_KEY_ON_PEAK_RATE,
-    XML_KEY_TIER_THRESHOLD,
-    XML_KEY_TIER_1_RATE,
-    XML_KEY_TIER_2_RATE,
-    XML_KEY_SERVICE_CHARGE,
-    XML_KEY_LOSS_ADJUSTMENT_FACTOR,
-    XML_KEY_NETWORK_SERVICE_RATE,
-    XML_KEY_CONNECTION_SERVICE_RATE,
-    XML_KEY_WHOLESALE_MARKET_SERVICE_RATE,
-    XML_KEY_RURAL_REMOTE_RATE_PROTECTION_CHARGE,
-    XML_KEY_STANDARD_SUPPLY_SERVICE,
+    XML_KEY_MONTHLY_CHARGE,
+    XML_KEY_GAS_SUPPLY_CHARGE,
+    XML_KEY_GAS_SUPPLY_CHARGE_PRICE_ADJUSTMENT,
+    XML_KEY_TRANSPORTATION_CHARGE_PRICE_ADJUSTMENT,
+    XML_KEY_DELIVERY_CHARGE_PRICE_ADJUSTMENT,
+    XML_KEY_FACILITY_CARBON_CHARGE,
+    XML_KEY_FEDERAL_CARBON_CHARGE,
+    XML_KEY_TRANSPORTATION_CHARGE,
     XML_KEY_GST,
-    XML_KEY_REBATE,
-    XML_KEY_ONE_TIME_FIXED_CHARGE,
 )
 
 
@@ -41,22 +34,16 @@ class OntarioEnergyBoardDataUpdateCoordinator(DataUpdateCoordinator):
     """Coordinator to manage Ontario Energy Board data."""
 
     _timeout = 10
-    off_peak_rate = None
-    mid_peak_rate = None
-    on_peak_rate = None
-    tier_threshold = None
-    tier_1_rate = None
-    tier_2_rate = None
-    service_charge = None
-    loss_adjustment_factor = None
-    network_service_rate = None
-    connection_service_rate = None
-    wholesale_market_service_rate = None
-    rural_remote_rate_protection_charge = None
-    standard_supply_service = None
+    monthly_charge = None
+    gas_supply_charge = None
+    gas_supply_charge_price_adjustment = None
+    transportation_charge_price_adjustment = None
+    delivery_charge_price_adjustment = None
+    facility_carbon_charge = None
+    federal_carbon_charge = None
+    transportation_charge = None
     gst = None
-    rebate = None
-    one_time_fixed_charge = None
+
 
     def __init__(self, hass: HomeAssistant) -> None:
         super().__init__(
@@ -81,42 +68,21 @@ class OntarioEnergyBoardDataUpdateCoordinator(DataUpdateCoordinator):
         content = await response.text()
         tree = ET.fromstring(content)
 
-        for company in tree.findall("BillDataRow"):
-            current_company = "{company_name} ({company_class})".format(
+        for company in tree.findall("GasBillData"):
+            current_company = "{company_name}".format(
                 company_name=company.find("Dist").text,
-                company_class=company.find("Class").text,
             )
             if current_company == self.energy_company:
-                self.off_peak_rate = float(company.find(XML_KEY_OFF_PEAK_RATE).text)
-                self.mid_peak_rate = float(company.find(XML_KEY_MID_PEAK_RATE).text)
-                self.on_peak_rate = float(company.find(XML_KEY_ON_PEAK_RATE).text)
-                self.tier_threshold = float(company.find(XML_KEY_TIER_THRESHOLD).text)
-                self.tier_1_rate = float(company.find(XML_KEY_TIER_1_RATE).text)
-                self.tier_2_rate = float(company.find(XML_KEY_TIER_2_RATE).text)
-                self.service_charge = float(company.find(XML_KEY_SERVICE_CHARGE).text)
-                self.loss_adjustment_factor = float(
-                    company.find(XML_KEY_LOSS_ADJUSTMENT_FACTOR).text
-                )
-                self.network_service_rate = float(
-                    company.find(XML_KEY_NETWORK_SERVICE_RATE).text
-                )
-                self.connection_service_rate = float(
-                    company.find(XML_KEY_CONNECTION_SERVICE_RATE).text
-                )
-                self.wholesale_market_service_rate = float(
-                    company.find(XML_KEY_WHOLESALE_MARKET_SERVICE_RATE).text
-                )
-                self.rural_remote_rate_protection_charge = float(
-                    company.find(XML_KEY_RURAL_REMOTE_RATE_PROTECTION_CHARGE).text
-                )
-                self.standard_supply_service = float(
-                    company.find(XML_KEY_STANDARD_SUPPLY_SERVICE).text
-                )
+                self.monthly_charge = float(company.find(XML_KEY_MONTHLY_CHARGE).text)
+                self.gas_supply_charge = float(company.find(XML_KEY_GAS_SUPPLY_CHARGE).text)
+                self.gas_supply_charge_price_adjustment = float(company.find(XML_KEY_GAS_SUPPLY_CHARGE_PRICE_ADJUSTMENT).text)
+                self.transportation_charge_price_adjustment = float(company.find(XML_KEY_TRANSPORTATION_CHARGE_PRICE_ADJUSTMENT).text)
+                self.delivery_charge_price_adjustment = float(company.find(XML_KEY_DELIVERY_CHARGE_PRICE_ADJUSTMENT).text)
+                self.facility_carbon_charge = float(company.find(XML_KEY_FACILITY_CARBON_CHARGE).text)
+                self.federal_carbon_charge = float(company.find(XML_KEY_FEDERAL_CARBON_CHARGE).text)
+                self.transportation_charge = float(company.find(XML_KEY_TRANSPORTATION_CHARGE).text)
                 self.gst = float(company.find(XML_KEY_GST).text)
-                self.rebate = float(company.find(XML_KEY_REBATE).text)
-                self.one_time_fixed_charge = float(
-                    company.find(XML_KEY_ONE_TIME_FIXED_CHARGE).text
-                )
+            
                 return
 
         self.logger.error("Could not find energy rates for %s", self.energy_company)
