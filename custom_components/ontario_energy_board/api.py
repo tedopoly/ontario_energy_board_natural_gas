bb14ae161662ce865254c12c0d5af217c46ebcce
tedopoly <93625527+tedopoly@users.noreply.github.com>
2023-11-13 07:40:04 -0500

    Update api.py

--- custom_components/ontario_energy_board/api.py
+++ custom_components/ontario_energy_board/api.py
@@ -34,11 +34,10 @@ async def get_energy_companies() -> list[str]:
     tree = ET.fromstring(content)
 
     all_companies = [
-        "{company_name} ({company_class})".format(
+        "{company_name}".format(
             company_name=company.find("Dist").text,
-            company_class=company.find("Class").text,
         )
-        for company in tree.findall("BillDataRow")
+        for company in tree.findall("GasBillData")
     ]
     all_companies.sort()
 
@@ -67,10 +66,9 @@ class OntarioEnergyBoard:
         content = await response.text()
         tree = ET.fromstring(content)
 
-        for company in tree.findall("BillDataRow"):
-            current_company = "{company_name} ({company_class})".format(
+        for company in tree.findall("GasBillData"):
+            current_company = "{company_name}".format(
                 company_name=company.find("Dist").text,
-                company_class=company.find("Class").text,
             )
             if current_company == self.energy_company:
                 self.off_peak_rate = float(company.find(XML_KEY_OFF_PEAK_RATE).text)
@@ -80,38 +78,6 @@ class OntarioEnergyBoard:
 
         _LOGGER.error("Could not find energy rates for %s", self.energy_company)
 
-    @property
-    def active_peak(self):
-        """
-        Find the active peak based on the current day and hour.
 
-        According to OEB, weekends and holidays are 24-hour off peak periods.
-        During summer (observed from May 1st to Oct 31st), the morning and evening
-        periods are mid-peak, and the afternoon is on-peak. This flips during winter
-        time, where morning and evening are on-peak and afternoons mid-peak.
-        """
-        current_time = as_local(now())
 
-        is_summer = (
-            date(current_time.year, 5, 1)
-            <= current_time.date()
-            <= date(current_time.year, 10, 31)
-        )
-        is_holiday = current_time.date() in self.ontario_holidays
-        is_weekend = current_time.weekday() >= 5
 
-        if is_holiday or is_weekend:
-            return STATE_OFF_PEAK
-
-        current_hour = int(current_time.strftime("%H"))
-        if (7 <= current_hour < 11) or (17 <= current_hour < 19):
-            return STATE_MID_PEAK if is_summer else STATE_ON_PEAK
-        if 11 <= current_hour < 17:
-            return STATE_ON_PEAK if is_summer else STATE_MID_PEAK
-
-        return STATE_OFF_PEAK
-
-    @property
-    def active_peak_rate(self):
-        """Returns the current peak's rate."""
-        return getattr(self, f"{self.active_peak}_rate")
